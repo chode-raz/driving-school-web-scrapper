8429936cb297f691141ffebc0c2fd5d8371b8a0a
Rastislav <detko.rastislav93@gmail.com>
2020-10-13 21:55:17 +0200

    add download pages assets

--- scraper.py
+++ scraper.py
@@ -9,6 +9,7 @@ import fileinput
 class ScrapIt:
     base_url = 'https://extranet.jiscd.sk'
 
+    assets_path = '/assets/'
     pages_path = '/home/rastislav/PycharmProjects/drive_school_scraper/test/'
 
     forbidden_urls = ['',
@@ -17,11 +18,11 @@ class ScrapIt:
 
     def __init__(self):
         jar = requests.cookies.RequestsCookieJar()
-        jar.set('LastMRH_Session', '3c253e69')
-        jar.set('MRHSession', 'de9d929e38cc248b9383cc663c253e69')
-        jar.set('_WL_AUTHCOOKIE_SAKAI_JSESSIONID', 'a0mV5E1BPmLoxLGcWGOl.')
-        jar.set('JSESSIONID', '824fc494-553a-4da2-b946-6e972e72edeb.sakaiext1a.jiscd')
-        jar.set('F5_ST', '1z1z1z1602240306z43200')
+        jar.set('LastMRH_Session', '93a79448')
+        jar.set('MRHSession', 'd42d8987e6e895c6629ca39b93a79448')
+        jar.set('_WL_AUTHCOOKIE_SAKAI_JSESSIONID', 'Ws9Oy94KIc.U9Tg6Tv4j')
+        jar.set('JSESSIONID', 'fdce9aae-4a90-4052-ad07-b41496bc2616.sakaiext1a.jiscd')
+        jar.set('F5_ST', '1z1z1z1602604338z43200')
 
         self.session = requests.Session()
         self.session.cookies = jar
@@ -42,7 +43,7 @@ class ScrapIt:
         containers = soup.select('.textType')
         for container in containers:
             texts = container.select('.textbox span')
-            title = ''
+            title = ""
             for text in texts:
                 if "Lekcia" in text.get_text():
                     title = text.get_text()
@@ -57,11 +58,13 @@ class ScrapIt:
                     urls.append((header, url))
                     dir_path = self.pages_path + title + '/' + str(i) + '. ' + header.replace('/', ' ')
                     file_path = dir_path + '/' + header.replace('/', ' ') + '.html'
-                    if not os.path.exists(file_path):
-                        if url is not None and url not in self.forbidden_urls:
+                    if url is not None and url not in self.forbidden_urls:
+                        if not os.path.exists(file_path):
                             i += 1
                             self.create_dir(dir_path)
                             self.get_page(url, file_path)
+                        self.get_page_assets(file_path, dir_path)
+
         pass
 
     def create_dir(self, dir_name):
@@ -79,61 +82,76 @@ class ScrapIt:
 
     pass
 
+    def create_assets_folder(self, path):
+        self.create_dir(path + self.assets_path)
+        pass
 
-def create_assets_folder(self, path):
-    self.create_dir(path + '/assets/')
-    pass
+    def get_page_assets(self, page_path, dir_path):
+        self.create_assets_folder(dir_path)
 
+        with open(page_path, 'r+') as page_file:
+            page = page_file.read()
+        page_file.close()
 
-def get_page_assets(self, page_path, storage_path):
-    with open(page_path) as file:
-        page = file.read()
-    file.close()
+        soup = BeautifulSoup(page, "html.parser")
 
-    file_input = fileinput.input(page_path, inplace=True)
+        for link in soup.findAll('img'):
+            image_url = link.get('src')
+            image_name = image_url.split('/')[-1]
+            new_path = dir_path + self.assets_path + image_name
+            relative_path = '.' + self.assets_path + image_name
+            link['src'] = relative_path
 
-    soup = BeautifulSoup(page, "html.parser")
+            if 'http' not in image_url:
+                image_url = self.base_url + image_url
 
-    for link in soup.findAll('img'):
-        image_url = link.get('src')
-        response = self.session.get(image_url, stream=True)
-        if response.status_code == 200:
-            with open(storage_path + image_url.split('/')[-1], 'wb') as file:
-                file.write(response.content)
-
-    for script in soup.findAll('script'):
-        script_type = script.get('type')
-
-        if script_type is not None and script_type == 'text/javascript' and script.get('src') is not None:
-            script_url = script.get('src')
-
-            if 'http' not in script_url:
-                script_url = self.base_url + script_url
-
-            response = self.session.get(script_url)
+            response = self.session.get(image_url, stream=True)
             if response.status_code == 200:
-                with open(storage_path + script_url.split('/')[-1], 'wb') as file:
+                with open(new_path, 'wb') as file:
                     file.write(response.content)
+                file.close()
 
-    for link in soup.findAll('link'):
-        link_type = link.get('type')
+        for script in soup.findAll('script'):
+            script_type = script.get('type')
 
-        if link_type == 'text/css':
-            link_url = link.get('href')
+            if script_type is not None and script_type == 'text/javascript' and script.get('src') is not None:
+                script_url = script.get('src')
+                script_name = script_url.split('/')[-1]
+                new_path = dir_path + self.assets_path + script_name
+                relative_path = '.' + self.assets_path + script_name
+                script['src'] = relative_path
 
-            if 'http' not in link_url:
-                link_url = self.base_url + link_url
+                if 'http' not in script_url:
+                    script_url = self.base_url + script_url
 
-            response = self.session.get(link_url)
-            if response.status_code == 200:
-                with open(storage_path + link_url.split('/')[-1], 'wb') as file:
-                    file.write(response.content)
+                response = self.session.get(script_url)
+                if response.status_code == 200:
+                    with open(new_path, 'wb') as file:
+                        file.write(response.content)
+                    file.close()
 
-    pass
+        for link in soup.findAll('link'):
+            link_type = link.get('type')
 
+            if link_type == 'text/css':
+                link_url = link.get('href')
 
-def replace_url(self, old_element: BeautifulSoup):
-    new_path = './assets/'
+                if 'http' not in link_url:
+                    link_url = self.base_url + link_url
 
-    new_element = BeautifulSoup('', 'xml')
-    pass
+                link_name = link_url.split('/')[-1]
+                new_path = dir_path + self.assets_path + link_name
+                relative_path = '.' + self.assets_path + link_name
+                link['href'] = relative_path
+
+                response = self.session.get(link_url)
+                if response.status_code == 200:
+                    with open(new_path, 'wb') as file:
+                        file.write(response.content)
+                    file.close()
+
+        with open(page_path, 'w') as page_file:
+            page_file.write(str(soup))
+        page_file.close()
+
+        pass
